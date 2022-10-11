210e0647ffb1a11dc95f305ebc7a0d2eaaed3ba5
Richard Korn <richo.korn@gmail.com>
2022-10-11 13:49:06 +0200

    [live] - Add 'checkin' feature

--- apps/luggage/src/app/app.module.ts
+++ apps/luggage/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { LuggageFeatureCheckinModule } from '@flight-workspace/luggage/feature-checkin';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, LuggageFeatureCheckinModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
